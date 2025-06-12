4ca4a5ec733cad0f9a0a71df86e19ba09191aec0
nikhilpatil79 <nikseven9@gmail.com>
2025-06-12 20:19:17 +0530

    Few changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,3 +49,19 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
 });
+let lastScrollTop = 0;
+  const header = document.querySelector('.main-header');
+
+  window.addEventListener('scroll', function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+      // Scrolling down
+      header.style.top = "-100px";
+    } else {
+      // Scrolling up
+      header.style.top = "0";
+    }
+
+    lastScrollTop = scrollTop;
+  });
